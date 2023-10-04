d8b1e4c0f2b3d8ac8a0fbd11ff60892880834530
Andre Bagestan <andrebagestan@gmail.com>
2023-10-04 17:15:23 -0300

    Melhoria no side menu para não permitir que duas abas estejam abertas ao mesmo tempo

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -6,9 +6,28 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
-  isCollapsed = true;
+export class MainComponent implements OnInit, OnChanges {
+  isCollapsed = false;
+
+  openMap: { [name: string]: boolean } = {
+    sub1: false,
+    sub2: false,
+    sub3: false,
+    sub4: false,
+  };
+
+  openHandler(value: string): void {
+    for (const key in this.openMap) {
+      if (key !== value) {
+        this.openMap[key] = false;
+      }
+    }
+  }
 
   constructor(public auth: AuthService) {}
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('🚀 ~ changes:', changes);
+  }
 }
